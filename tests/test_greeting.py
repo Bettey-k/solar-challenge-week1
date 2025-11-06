4af700883ee4bb3bb450ace03d455f331273a62b
betelhem <betelhemadu14@gmail.com>
2025-11-06 20:50:54 +0100

    CI

--- tests/test_greeting.py
+++ tests/test_greeting.py
@@ -1,15 +1,11 @@
-# tests/test_greeting.py
 import sys
 import os
-
-# flake8: noqa: E402  👈 Add this to tell flake8 to ignore import order in this file
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-from src.greeting import get_greeting
-
 
 def test_get_greeting():
+    from src.greeting import get_greeting  # local import ✅
     assert get_greeting("Betelhem") == "Hello, Betelhem!"
